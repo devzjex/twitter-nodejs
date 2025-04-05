e761e87bbb4fa1d67afe41cd641f803ba669852e
devzjex <dungthemai20@gmail.com>
2025-04-05 17:05:56 +0700

    connect database and create collection mongo

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express'
+import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
   if (email === 'devmai@gmail.com' && password === '123123') {
     res.status(200).json({ message: 'Logged in successfully' })
-    return
   }
   res.status(400).json({ message: 'Login failed' })
-  return
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await usersService.register({ email, password })
+    res.json({ message: 'Register success', result })
+  } catch (error) {
+    res.status(400).json({ message: 'Register failed' })
+  }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { loginController } from '~/controllers/user.controllers'
+import { loginController, registerController } from '~/controllers/user.controllers'
 import { loginValidator } from '~/middlewares/users.middlewares'
 
 const userRouter = Router()
 
 userRouter.post('/login', loginValidator, loginController)
+userRouter.post('/register', registerController)
 
 export default userRouter
